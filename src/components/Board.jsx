6ab2a3cdb6aefeb87bb2883e01608c63cf3d0fec
John Chun <johnchun@Johns-Mac-mini.attlocal.net>
2025-04-13 16:09:08 -0700

    finished with logic

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,17 +1,23 @@
 import Square from "./Square"
 export default function Board(props){
-    const { onClick } = props
+    const { squares, onClick } = props
+    const style = {
+        border: '4px solid darkblue',
+        borderRadius: '10px',
+        width: '250px',
+        height: '250px',
+        margin: '0 auto',
+        display: 'grid',
+        gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)'
+    };
     return (
-        <div className="board">
-            <Square value="1" onClick={onClick}/>
-            <Square value="2" onClick={onClick}/>
-            <Square value="3" onClick={onClick}/>
-            <Square value="4" onClick={onClick}/>  
-            <Square value="5" onClick={onClick}/>  
-            <Square value="6" onClick={onClick}/>
-            <Square value="7" onClick={onClick}/>
-            <Square value="8" onClick={onClick}/>
-            <Square value="9" onClick={onClick}/>           
+        <div style={style} className="board">
+            {squares.map((square, i) => (
+              
+                    <Square key={i} value={square} onClick={() => onClick(i)}/>
+                
+            ))}
+                      
         </div>
     )
 }
